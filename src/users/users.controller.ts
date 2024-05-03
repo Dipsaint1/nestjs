f88a3e6c7150e21f38c231aeb84770cf300be401
cyber9ja <ayodejioladapo15@gmail.com>
2024-05-03 16:04:22 +0100

    minor update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // /users 
+export class UsersController {
+  /* 
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+  */
+
+    @Get()  // GET /users
+    findAll(){
+      return []
+    }
+
+    // @Get('interns') //GET /users/interns
+    // findAllInterns(){
+    //   return []
+    // }
+
+    @Get(':id')  // GET /users/:id
+    findOne(@Param('id') id: string){
+      return { id }
+    }
+
+    @Post()  // POST /users
+    create(@Body() user: {}){
+      return user
+    }
+
+    @Patch('id') // PATCH /users/:id
+    update(@Param('id') id: string, @Body() user: {}){
+      return { id, user }
+    }
+}
