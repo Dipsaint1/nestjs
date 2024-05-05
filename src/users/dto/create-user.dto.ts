24c9bb1edeaddfff6db388497d63463ab0bda9a3
cyber9ja <ayodejioladapo15@gmail.com>
2024-05-05 12:21:01 +0100

    Added ValidationPipe to controllers

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsEnum, IsString } from "class-validator";
+
 export class CreateUserDto{
+  @IsString()
   name: string;
+  
+  @IsEmail()
   email: string;
+
+  @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'], {
+    message: 'Valid role required',
+  })
   role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,40 +8,46 @@ import {
   Post,
   Query,
   ParseIntPipe,
+  ValidationPipe
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users') // /users
+@Controller('users') //       /users
 export class UsersController {
   constructor(private readonly usersService: UsersService) {} //Singleton
 
-  @Get() // GET /users or /users?role=value&age=value
-  findAll(
+  @Get() // GET               /users or /users?role=value&age=value
+  async findAll(
     @Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN',
     @Query('age') age?: number,
   ) {
     return this.usersService.findAll(role);
   }
 
-  @Get(':id') // GET /users/:id
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id); // Method 2
+  @Get(':id') // GET        /users/:id
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id); 
   }
 
-  @Post() // POST /users
-  create( @Body() createUserDto: CreateUserDto ) {
+  @Post() // POST          /users
+  // ValidationPipe ensures that the body parameters are validated 
+  // against the Dto's and get a message if there is an error
+  async create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Patch(':id') // PATCH /users/:id
-  update( @Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto ) {
+  @Patch(':id') // PATCH    /users/:id
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+  ) {
     return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete(':id') // DELETE /users/:id
-  deleteOne(@Param('id', ParseIntPipe) id: number) {
+  @Delete(':id') // DELETE    /users/:id
+  async deleteOne( @Param('id', ParseIntPipe) id: number ) {
     return this.usersService.delete(id);
   }
 }
